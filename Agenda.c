b8364c4438cc600db28e1c5390f5f5b1f6cf47f9
MidhiaQ <queirozmidhia9@gmail.com>
2024-04-02 11:03:44 -0300

    comentando o código

--- Agenda.c
+++ Agenda.c
@@ -10,8 +10,10 @@ typedef struct Contato {
     char email[50];
 } Pessoa;
 
-Pessoa agenda[TAM];
+Pessoa agenda[TAM]; 
+// Declaração da tabela de contatos
 
+// Função para inicializar a tabela de contatos
 void inicializarTabela(Pessoa tabela[]) {
     int i;
     for(i = 0; i < TAM; i++){
@@ -21,10 +23,12 @@ void inicializarTabela(Pessoa tabela[]) {
     }
 }
 
+// Função de dispersão para chaves inteiras
 int funcaoHash(int chave) {
      return chave % TAM;
 }
 
+// Função de dispersão para strings
 int funcaoHashString( char str[]){
     int i,tamS = strlen(str);
     unsigned int hash = 0;
@@ -34,6 +38,7 @@ int funcaoHashString( char str[]){
     return hash % TAM;
 }
 
+// Função para ler contatos de um arquivo
 void lerArquivo(Pessoa tabela[]) {
     FILE *arq = fopen("todosOsContatos.txt", "r");
     if (arq == NULL) {
@@ -43,31 +48,34 @@ void lerArquivo(Pessoa tabela[]) {
     fseek(arq, 0, SEEK_SET); 
     Pessoa contato;
     while (fscanf(arq, "Nome: %[^\n], Telefone: %[^\n], Email: %[^\n]\n", contato.nome, contato.telefone, contato.email) == 3)
-        inserir(tabela, contato);
+        inserir(tabela, contato); // Insere o contato lido na tabela
     fclose(arq);
 }
 
+// Função para inserir um contato na tabela
 void inserir(Pessoa tabela[], Pessoa p) {
     int id =  funcaoHashString(p.nome);
     while (strlen(tabela[id].nome) > 0) {
-        id = (id + 1) % TAM;
+        id = (id + 1) % TAM; // Tratamento de colisões por sondagem linear
     }
-    tabela[id] = p;
+    tabela[id] = p; // Insere o contato na tabela
 }
 
+// Função para buscar um contato na tabela
 Pessoa *buscar(Pessoa tabela[], char chave[]) {
     int id = funcaoHashString(chave);
     printf("\nIndice gerada: %d\n",id);
     while (strlen(tabela[id].nome) > 0){
         if(strcmp(tabela[id].nome,chave) == 0){
-            return &tabela[id];
+            return &tabela[id]; // Retorna o endereço do contato se encontrado
         }else{
-            id = funcaoHash(id +1);
+            id = funcaoHash(id +1); // Tratamento de colisões por sondagem linear
         }
     }
-    return NULL;   
+    return NULL;   // Retorna NULL se o contato não for encontrado
 }
 
+// Função para escrever todos os contatos da tabela em um arquivo
 void escreverArquivo(Pessoa tabela[]) {
     FILE *arq = fopen("todosOsContatos.txt", "a");
     if (arq == NULL) {
@@ -85,6 +93,7 @@ void escreverArquivo(Pessoa tabela[]) {
     fclose(arq);
 }
 
+// Função para imprimir todos os contatos da tabela
 void imprimir(Pessoa tabela[]) {
     int i;
     printf("\n");
@@ -98,8 +107,8 @@ int main() {
     int opcao;
     Pessoa tabela[TAM], *pessoaBuscada;
     char nome[50], telefone[15], email[50];
-    inicializarTabela(tabela);
-    lerArquivo(tabela);
+    inicializarTabela(tabela); // Inicializa a tabela de contatos
+    lerArquivo(tabela); // Lê os contatos do arquivo
 
     do {
         printf("\n0 - sair\n1 - inserir\n2 - buscar\n3 - imprimir\n\n");
@@ -118,13 +127,13 @@ int main() {
             strcpy(p.nome, nome);
             strcpy(p.telefone, telefone);
             strcpy(p.email, email);
-            inserir(tabela, p);
+            inserir(tabela, p); // Insere o contato na tabela
             break;
 
         case 2:
             printf("Qual o nome do contato que deseja buscar: ");
             fgets(nome, 49, stdin);
-            pessoaBuscada = buscar(tabela, nome);
+            pessoaBuscada = buscar(tabela, nome); // Busca o contato na tabela
             if (pessoaBuscada != NULL) {
                 printf("Nome: %s\nTelefone: %s\nEmail: %s\n", pessoaBuscada->nome, pessoaBuscada->telefone, pessoaBuscada->email);
             } else {
@@ -132,7 +141,7 @@ int main() {
             }
             break;
         case 3:
-            imprimir(tabela);
+            imprimir(tabela); // Imprime todos os contatos da tabela
             break;
         default:
             printf("Opcao invalida\n");
@@ -140,7 +149,7 @@ int main() {
         }
     } while(opcao != 0);
 
-    escreverArquivo(tabela);
+    escreverArquivo(tabela); // Escreve os contatos da tabela no arquivo
 
     return 0;
 }
